ab27012c2568ed8141db74fdf9d426044e2fea69
Nico Baier <nico.baier@gmail.com>
2025-11-18 18:00:02 -0600

    sync

---
+++ src/scrapers/sites/farmtojar.ts
@@ -0,0 +1,23 @@
+/**
+ * FarmToJar scraper
+ * https://farmtojar.com/
+ *
+ * Custom implementation - has overridden methods
+ * TODO: Review Python implementation and port custom logic
+ */
+
+import { AbstractScraper } from "../abstract";
+
+export class FarmToJarScraper extends AbstractScraper {
+	host(): string {
+		return "farmtojar.com";
+	}
+
+	/**
+	 * TODO: Implement custom author() logic
+	 * Check Python implementation in recipe_scrapers/farmtojar.py
+	 */
+	// author(): ReturnType {
+	// 	return undefined;
+	// }
+}
